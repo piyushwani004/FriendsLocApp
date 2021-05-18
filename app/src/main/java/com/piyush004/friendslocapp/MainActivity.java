9f5601637afd9832f2abd5890ac49e7d17bd6b2c
Piyush wani <piyushwani04@gmail.com>
2021-05-18 19:56:42 +0530

    Others File Changes

--- app/src/main/java/com/piyush004/friendslocapp/MainActivity.java
+++ app/src/main/java/com/piyush004/friendslocapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.piyush004.friendslocapp;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+
+           /* if (firebaseAuth.getCurrentUser() != null) {
+
+            } else {
+
+            }*/
+
+        }, SPLASH_SCREEN_TIME_OUT);
+
+
     }
 }
