e56013fb88a03fa75529cff4361f7fba1ebb3797
Piyush wani <piyushwani04@gmail.com>
2021-05-18 23:06:30 +0530

    Add Home Activity file.

---
+++ app/src/main/java/com/piyush004/friendslocapp/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.piyush004.friendslocapp.Home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.piyush004.friendslocapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
