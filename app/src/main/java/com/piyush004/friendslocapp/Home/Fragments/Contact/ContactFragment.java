fa299ec2c4521cad3d1b70555fdd9c89e1009f9c
Piyush wani <piyushwani04@gmail.com>
2021-06-10 10:15:49 +0530

    Revert "New Bug Fixs"
    This reverts commit 39f1b17e17946badd1d85faa493c74c037d95449.

--- app/src/main/java/com/piyush004/friendslocapp/Home/Fragments/Contact/ContactFragment.java
+++ app/src/main/java/com/piyush004/friendslocapp/Home/Fragments/Contact/ContactFragment.java
@@ -54,7 +54,6 @@ public class ContactFragment extends Fragment {
     private SwipeRefreshLayout swipeRefreshLayout;
     private Context context;
     private SearchView searchView;
-    Handler myHandler = new Handler();
 
     public ContactFragment() {
         // Required empty public constructor
@@ -94,7 +93,7 @@ public class ContactFragment extends Fragment {
         newList = (ArrayList<ContactModel>) removeDuplicateNumber(phoneContact);
         Log.e(TAG, "newList  Size : " + newList.size());
 
-
+        //store firebase user in firebase list...
         readFirebaseData(list -> {
             firebaseList = list;
             if (firebaseList.size() > 0) {
@@ -111,8 +110,6 @@ public class ContactFragment extends Fragment {
                 } else {
                     Toast.makeText(getContext(), "No Contact found", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(getContext(), "No Data found", Toast.LENGTH_SHORT).show();
             }
         });
 
