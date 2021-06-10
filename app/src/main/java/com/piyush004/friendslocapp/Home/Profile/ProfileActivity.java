a81e77e348c470d974598b4856e557e0793c2fa4
Piyush wani <piyushwani04@gmail.com>
2021-06-10 10:41:20 +0530

    Add Copyrights to Whole Project...

--- app/src/main/java/com/piyush004/friendslocapp/Home/Profile/ProfileActivity.java
+++ app/src/main/java/com/piyush004/friendslocapp/Home/Profile/ProfileActivity.java
@@ -1,3 +1,16 @@
+/*
+ * <!--
+ *   ~ /*******************************************************
+ *   ~  * Copyright (C) 2021-2031 {Piyush Wani and  Mayur Sapkale} <{piyushwani04@gmail.com}>
+ *   ~  *
+ *   ~  * This file is part of {FriendLocatorApp}.
+ *   ~  *
+ *   ~  * {FriendLocatorApp} can not be copied and/or distributed without the express
+ *   ~  * permission of {Piyush Wani and  Mayur Sapkale}
+ *   ~  ******************************************************
+ *   -->
+ */
+
 package com.piyush004.friendslocapp.Home.Profile;
 
 import android.app.ProgressDialog;
