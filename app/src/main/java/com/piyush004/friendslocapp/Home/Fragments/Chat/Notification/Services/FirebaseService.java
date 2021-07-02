2303858da3adf443816488acc3314ed03123ea3f
Piyush wani <piyushwani04@gmail.com>
2021-07-02 12:10:32 +0530

    update FirebaseService.java

--- app/src/main/java/com/piyush004/friendslocapp/Home/Fragments/Chat/Notification/Services/FirebaseService.java
+++ app/src/main/java/com/piyush004/friendslocapp/Home/Fragments/Chat/Notification/Services/FirebaseService.java
@@ -13,25 +13,39 @@
 
 package com.piyush004.friendslocapp.Home.Fragments.Chat.Notification.Services;
 
+import android.util.Log;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.piyush004.friendslocapp.Home.Fragments.Chat.Notification.Token;
 
 public class FirebaseService extends FirebaseMessagingService {
 
+    private static final String TAG = FirebaseService.class.getSimpleName();
 
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
+        Log.e(TAG, "onNewToken: " + s);
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        String refreshToken = FirebaseInstanceId.getInstance().getToken();
-        if (firebaseUser != null) {
-            updateToken(refreshToken);
-        }
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                        return;
+                    }
+
+                    String token = task.getResult();
+                    Log.e(TAG, "FirebaseMessaging :" + token);
+                    if (firebaseUser != null) {
+                        updateToken(token);
+                    }
+
+                });
     }
 
     private void updateToken(String refreshToken) {
